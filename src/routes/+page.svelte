578ae8fe83686237909af2e8545143f19d160936
Maaike Verplancke <maaike.verplancke@hva.nl>
2023-09-14 20:49:01 +0200

    Begin styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,33 +1,33 @@
-<h1>Starlight Diviners</h1>
 <script>
-    import * as prismic from '@prismicio/client'
-    import { SliceZone } from '@prismicio/svelte'
-    import { components } from '$lib/slices'
-  
-    export let data
-    let members = data.list
-    console.log(data )
- 
- </script>
+  import * as prismic from "@prismicio/client";
+  import { SliceZone } from "@prismicio/svelte";
+  import { components } from "$lib/slices";
+
+  export let data;
+  let members = data.list;
+  console.log(data);
+</script>
+
+<h1>Starlight Diviners</h1>
 
 <section>
   <!-- //for each loop svelte syntax// -->
-  {#each members as member }
-  <article class="members-card">
-   <h2>{@html prismic.asHTML(member.data.name)}</h2> 
-   <p>Leeftijd: {(member.data.age)}</p> 
-   <p>{(member.data.horoscope)}</p>
-   <p>{@html prismic.asHTML(member.data.namefavoriteCodeLanguage)}</p>
-</article>
-{/each}
-
-
+  {#each members as member}
+    <article class="members-card">
+      <h2>{@html prismic.asHTML(member.data.name)}</h2>
+      <p>Leeftijd: {member.data.age}</p>
+      <p>{member.data.horoscope}</p>
+      <p>{@html prismic.asHTML(member.data.namefavoriteCodeLanguage)}</p>
+      <button>Visitekaartje</button>
+    </article>
+  {/each}
 </section>
 
 <style>
-.members-card{
-  border: 1px solid black;
-  margin: 1em auto;
-}
+  .members-card {
+      border-style: solid;
+      border-width: 1px;
+      border-color: var(--gold);
+  }
 
 </style>
