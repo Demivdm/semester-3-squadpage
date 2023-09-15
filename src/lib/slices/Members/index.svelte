63484b0e8312baed01f6163ae3e07d26b7023caa
Maaike Verplancke <maaike.verplancke@hva.nl>
2023-09-15 14:58:38 +0200

    Styling apart
    Niet in +layout.svelte

--- src/lib/slices/Members/index.svelte
+++ src/lib/slices/Members/index.svelte
@@ -4,26 +4,64 @@
   console.log(slice);
 </script>
 
-<section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
-
-{#each slice.items as item}
-  <article class="member-card">
-          <h2>{item.squadmembers.data.name[0].text}</h2>
-          <img src="{item.squadmembers.data.memberimage.url}" alt="{item.squadmembers.data.name[0].text}">
-          <img src="{item.squadmembers.data.horoscopeimage.url}" alt="{item.squadmembers.data.horoscope}">
-          <p>{item.squadmembers.data.age}</p>
-          <p>{item.squadmembers.data.horoscope}</p>
-          <p>{item.squadmembers.data.favoritecodelanguage[0].text}</p>
-          <a href="{item.squadmembers.data.profielcardlink}">Visitekaartje</a> 
-    
-  </article> 
-{/each}
+<section
+  data-slice-type={slice.slice_type}
+  data-slice-variation={slice.variation}
+>
+  {#each slice.items as item}
+    <article class="member-card">
+      <h2>{item.squadmembers.data.name[0].text}</h2>
+      <img
+        src={item.squadmembers.data.memberimage.url}
+        alt={item.squadmembers.data.name[0].text}
+      />
+      <img
+        src={item.squadmembers.data.horoscopeimage.url}
+        alt={item.squadmembers.data.horoscope}
+      />
+      <p>{item.squadmembers.data.age}</p>
+      <p>{item.squadmembers.data.horoscope}</p>
+      <p>{item.squadmembers.data.favoritecodelanguage[0].text}</p>
+      <a href={item.squadmembers.data.profielcardlink}>Visitekaartje</a>
+    </article>
+  {/each}
 </section>
 
-  <style>
+<style>
+  :root {
+    --gold: #e1c253;
+    --darkblue: #061528;
+    --offwhite: #fffbea;
+    --lightblue: #062968;
+    background: url(background-stars.jpg);
+  }
 
-.member-card{
-  border: 1px solid black;
-  margin: 1em ;
-}
-  </style>
+  h1,
+  h2 {
+    font-family: "Elsie";
+    color: var(--offwhite);
+  }
+
+  p {
+    font-family: "Montserrat";
+    color: var(--offwhite);
+  }
+
+  a {
+    font-family: "Elsie";
+    padding: 0.7em;
+    width: 135px;
+    height: 35px;
+    /* background: rgb(202, 162, 82); */
+    background: linear-gradient(
+      90deg,
+      rgba(225, 194, 83, 1) 0%,
+      rgba(255, 231, 183, 1) 50%,
+      rgba(225, 194, 83, 1) 100%
+    );
+    color: var(--darkblue);
+    border-style: solid;
+    border-width: 1px;
+    border-color: var(--darkblue);
+  }
+</style>
