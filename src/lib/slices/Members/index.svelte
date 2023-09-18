a8b35a2f93e61594261d0a2e1488acdcb73d090d
Maaike Verplancke <maaike.verplancke@hva.nl>
2023-09-18 15:12:28 +0200

    Button aangepast
    Met een lijn erin

--- src/lib/slices/Members/index.svelte
+++ src/lib/slices/Members/index.svelte
@@ -22,7 +22,12 @@
       <p>{item.squadmembers.data.age}</p>
       <p>{item.squadmembers.data.horoscope}</p>
       <p>{item.squadmembers.data.favoritecodelanguage[0].text}</p>
-      <a href={item.squadmembers.data.profielcardlink}>Visitekaartje</a>
+
+      <div class="button_container">
+        <a href={item.squadmembers.data.profielcardlink} class="button"
+          >Visitekaartje</a
+        >
+      </div>
     </article>
   {/each}
 </section>
@@ -47,12 +52,7 @@
     color: var(--offwhite);
   }
 
-  a {
-    font-family: "Elsie";
-    padding: 0.7em;
-    width: 135px;
-    height: 35px;
-    /* background: rgb(202, 162, 82); */
+  .button_container {
     background: linear-gradient(
       90deg,
       rgba(225, 194, 83, 1) 0%,
@@ -63,5 +63,52 @@
     border-style: solid;
     border-width: 1px;
     border-color: var(--darkblue);
+    width: 215px;
+    height: 55px;
+    margin: 0 auto;
   }
+
+  a.button {
+    color: var(--darkblue);
+    border: 1px solid var(--darkblue);
+    width: 196px;
+    height: 40px;
+    padding: 10px 38px;
+    margin: 8px 1px -42px 8.5px;
+    cursor: pointer;
+    font-weight: bold;
+    font-size: 19px;
+    font-family: "Elsie";
+    display: inline-block;
+    text-decoration: none;
+    /* -webkit-transition: all 200ms ease 0s;
+    -moz-transition: all 200ms ease 0s;
+    -ms-transition: all 200ms ease 0s;
+    -o-transition: all 200ms ease 0s;
+    transition: all 200ms ease 0s;
+    -webkit-appearance: none;
+    -webkit-font-smoothing: antialiased;
+    font-smoothing: antialiased; */
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+  }
+
+  /* .button {
+    font-family: "Elsie";
+    padding: 0.7em;
+    width: 135px;
+    height: 35px;
+    /* background: rgb(202, 162, 82); */
+  /* background: linear-gradient(
+          90deg,
+          rgba(225, 194, 83, 1) 0%,
+          rgba(255, 231, 183, 1) 50%,
+          rgba(225, 194, 83, 1) 100%
+        );
+        color: var(--darkblue);
+        border-style: solid;
+        border-width: 1px;
+        border-color: var(--darkblue);
+      } */
 </style>
