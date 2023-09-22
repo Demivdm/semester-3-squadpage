a3ee5b2641e85e1d77c76061e4dd68bc288d7b68
Duneya <54691201+Duneyasaleh@users.noreply.github.com>
2023-09-22 14:36:20 +0200

    horoscope data opgehald

--- src/routes/[uid]/+page.server.js
+++ src/routes/[uid]/+page.server.js
@@ -2,11 +2,13 @@ import { createClient } from '$lib/prismicio'
 
 // export const prerender = true;
 
-export async function load({ params}) {
-//   const client = createClient({ fetch, request });
-// 
-//   const document = await client.getByUID('horoscoperad', params.uid);
+export async function load({ fetch, request, params}) {
+  let { uid } = params
+  const client = createClient({ fetch, request });
+
+  const document = await client.getByUID('horoscoperad', uid);
+  const members = await client.getAllByType('squadmembers')
   
-  return( params)
+  return { page: document.data , members: members }
   };
  
--- src/routes/[uid]/+page.svelte
+++ src/routes/[uid]/+page.svelte
@@ -2,13 +2,36 @@
   import { SliceZone } from "@prismicio/svelte";
   import { dev } from "$app/environment";
   import * as components from "$lib/slices";
+  import { onMount } from "svelte";
   export let data;
-  console.log(data);
+  // console.log(data);
+  // console.log(data.members)
+// onMount(() => {
+
+//   var elementsToShow = document.querySelectorAll('[value]');
+// // Loop through the selected elements
+// for (var i = 0; i < elementsToShow.length; i++) {
+//   var element = elementsToShow[i];
+  
+  // Check the value of the "data-show" attribute
+//   var showValue = element.getAttribute('value');
+//   console.log(showValue)
+//   // If the value is "true," display the element; otherwise, hide it
+//   if (showValue == 'schorpioen') {
+  
+//     element.style.display = 'block';
+//   }
+// }
+
+  // console.log(horoscope)
+  // }
+
+// });
 </script>
 
-<main>
+<main class="page-contaniar">
   <div class="button_container">
-    <a href="/" class="button"
+    <a href="/" class="back-button"
       ><svg
         xmlns="http://www.w3.org/2000/svg"
         width="25"
@@ -18,39 +41,49 @@
       >
         <path
           d="M7.99995 14.0323C8.00295 13.2903 7.27445 12.1793 6.53622 11.2463C5.58709 10.0425 4.45134 8.99088 3.14759 8.18659C2.17002 7.58362 0.984362 7.00382 0.0283704 6.99994M0.0283704 6.99994C0.984362 7.00382 2.17568 6.43364 3.1571 5.83861C4.46733 5.04391 5.61156 4.00154 6.57041 2.80741C7.31617 1.87942 8.05366 0.772402 8.05666 0.0324082M0.0283704 6.99994L24.0282 7.09716"
-          stroke="#E1C253"
+          stroke="#061528"
         />
-      </svg></a
+      </svg> Terug</a
     >
   </div>
-  <h1>{data.uid}</h1>
   <section class="desktop-naast-elkaar">
     <section class="card-introduction">
-      <img src="boogschutter-achterkant.png" class="card-back" />
+      <img src="{data.page.horoscopecard.url}" alt="{data.page.title[0].text}" />  
       <div class="horoscope-introduction">
-        <h2>Boogschutter</h2>
+        <h2>{data.page.title[0].text}</h2>
         <p>
-          De levendige en openhartige Boogschutters zijn geboren natuurlijke
-          leiders die achter alles aan gaan wat ze willen, ongeacht wat andere
-          mensen van hen denken. Dit sterrenbeeld houdt van reizen en avontuur.
-          Boogschutter is een optimistisch, positief denkend teken dat
-          geassocieerd wordt met de zoektocht naar vrijheid van alle
-          beperkingen, evenals idealisme, religie en filosofie.
+          {data.page.explanationtext}
         </p>
       </div>
     </section>
     <article class="students-same-horoscope">
       <h2>Studenten met hetzelde sterrenbeeld</h2>
-      <div class="cards-front">
-        <img src="Squad2A-Demi.jpg" />
-        <img src="Squad2A-Maaike.jpg" />
-        <img src="Squad2A-Ralph.jpg" />
-        <img src="Squad2A-Sander.jpg" />
-        <img src="Squad2A-Sanne.jpg" />
-        <img src="Squad2A-Rosa.jpg" />
-      </div>
-    </article>
-  </section>
+      <section class="cards-container">
+        {#each data.members as member}
+        <!-- if (member) -->
+       <section class="member-card" value="{member.data.horoscope}">
+          <section class="back-of-card">
+            <h2>{member.data.name[0].text}</h2>
+            <div class="top-card-img">
+              <img
+                src={member.data.memberimage.url}
+                alt={member.data.name[0].text}
+              />
+            </div>
+            <ul>
+              <li>{member.data.age}</li>
+              <li>{member.data.horoscope}</li>
+              <li>{member.data.favoritecodelanguage[0].text}</li>
+            </ul>
+            <div class="button_container">
+              <a href={member.data.profielcardlink} class="button"
+                >Visitekaartje</a
+              >
+            </div>
+          </section>
+        </section>
+
+{/each}
 </main>
 
 <style>
@@ -64,7 +97,10 @@
 
   main {
     padding: 0.7em;
+    max-width: 80vw;
+    margin: auto;
   }
+  
 
   h1,
   h2 {
@@ -77,24 +113,91 @@
     font-family: "montserrat";
     color: var(--offwhite);
   }
-  /* BUTTON */
-  .button_container {
-    background: var(--darkblue);
-    color: var(--gold);
-    border-style: solid;
-    border-width: 1px;
-    border-color: var(--gold);
-    width: 115px;
-    height: 55px;
+  
+  .card-introduction {
+    display: flex;
+    flex-direction: column;
+  }
+   .card-introduction img{
+   margin-top: 2rem;
   }
 
-  a.button {
-    color: var(--gold);
-    border: 1px solid var(--gold);
-    width: 95px;
+  .horoscope-introduction {
+    padding: 0.5em;
+    /* margin-top: -1.5em; */
+  }
+
+  .card-back {
+    width: 140px;
+  }
+
+  img {
+    width: 163px;
+  }
+  .cards-container{
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+
+  .member-card {
+    background-color: transparent;
+    height: 423px;
+    width: 234px;
+    perspective: 1000px;
+    cursor: pointer;
+    display: flex;
+    margin: 1em;
+    align-self: center;
+    padding: 1em;
+    margin: 1em;
+
+  }
+  .top-card-img {
+    overflow: hidden;
+    width: 100%;
+    height: 40%;
+  }
+  .top-card-img img {
+    width: 100%;
+    position: relative;
+    top: -2em;
+  }
+
+  .back-of-card {
+    height: 423px;
+    width: 234px;
+    border: 1px solid gold;
+    background: var(--darkblue);
+  }
+  ul > li {
+    color: white;
+    list-style-type: none;
+    text-align: left;
+    font-family: "Montserrat";
+  }
+  .button_container {
+    background: linear-gradient(
+      90deg,
+      rgba(225, 194, 83, 1) 0%,
+      rgba(255, 231, 183, 1) 50%,
+      rgba(225, 194, 83, 1) 100%
+    );
+    color: var(--darkblue);
+    border-style: solid;
+    border-width: 1px;
+    border-color: var(--darkblue);
+    width: 215px;
+    height: 55px;
+    /* margin: 0 auto; */
+  }
+a.back-button{
+    color: var(--darkblue);
+    border: 1px solid var(--darkblue);
+    width: 196px;
     height: 40px;
     padding: 10px 38px;
-    margin: 8px 1px -45px 9.5px;
+    margin: 8px 1px -42px 8.5px;
     cursor: pointer;
     font-weight: bold;
     font-size: 19px;
@@ -104,28 +207,23 @@
     -webkit-box-sizing: border-box;
     -moz-box-sizing: border-box;
     box-sizing: border-box;
-  }
-
-  .card-introduction {
-    display: grid;
-    grid-auto-flow: column;
-  }
-
-  .horoscope-introduction {
-    padding: 0.5em;
-    margin-top: -1.5em;
-  }
-
-  .card-back {
-    width: 140px;
-  }
-
-  /* .students-same-horoscope {
-		width: 45%;
-	} */
-
-  img {
-    width: 163px;
+}
+  a.button {
+    color: var(--darkblue);
+    border: 1px solid var(--darkblue);
+    width: 196px;
+    height: 40px;
+    padding: 10px 38px;
+    margin: 8px 1px -42px 8.5px;
+    cursor: pointer;
+    font-weight: bold;
+    font-size: 19px;
+    font-family: "Elsie";
+    display: inline-block;
+    text-decoration: none;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
   }
 
   @media screen and (min-width: 1000px) {
@@ -142,6 +240,15 @@
     .desktop-naast-elkaar {
       display: grid;
       grid-auto-flow: column;
+      margin-top: 4rem;
+    }
+    .card-introduction{
+      display: grid;
+    grid-auto-flow: column;
     }
   }
+
+  /* .member-card{
+    display: none;
+  } */
 </style>
